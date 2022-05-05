bcda078147bf41b4e7302e26b544edbd49fef140
Мещеряков Дмитрий Евгеньевич <milkdimka@gmail.com>
2022-05-05 15:25:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
 import gunicorn
-
-from modules.api_func.mongo_api import apiMongoConnect
 
 app = Flask(__name__, template_folder="templates")
 app.debug = True
@@ -9,16 +9,16 @@ app.debug = True
 
 @app.route('/')
 def hello_world():
+    response = requests.get('https://rifmu.ru/'+"кот")
+    contents_html = response.content
 
-    dt = apiMongoConnect.req()
-    dt_json = dt.json()
-    content = dt_json['documents']
-    return render_template('base.html', data=content)
+    soup = BeautifulSoup(contents_html, 'lxml')
+    text_arr = []
 
-@app.route('/p/<post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    dt = apiMongoConnect.req(method_name="findOne",method_type="POST",add_data={"filter": { "_id": { "$oid": post_id } }})
-    dt_json = dt.json()
-    content = dt_json['documents'][0]
-    return render_template('page.html', data=content)
+    for child in soup.ul.recursiveChildGenerator():
+
+        if child.name == 'a':
+
+            text_arr.append(child.text)
+
+    return {"data": text_arr}
